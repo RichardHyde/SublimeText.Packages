8449e518f0a12c82eb97e00b729cad8886fb5a3b
Richard Hyde <email@richardhyde.net>
2014-02-28 00:13:07 +0000

    Renamed AppendToClipboard to CopyAppendToClipboard and added CutAppendToClipboard

--- AppendToClipboard.py
+++ AppendToClipboard.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
 
-class AppendToClipboardCommand(sublime_plugin.TextCommand):
+class CopyAppendToClipboardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		selections = self.view.sel()
 		clipboard = ""
@@ -11,3 +11,14 @@ class AppendToClipboardCommand(sublime_plugin.TextCommand):
 
 		sublime.set_clipboard(clipboard)
 
+class CutAppendToClipboardCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		selections = self.view.sel()
+		clipboard = ""
+		for sel in selections:
+			clipboard = clipboard + self.view.substr(sel)
+			self.view.replace(edit, sel,  "")
+
+		clipboard = sublime.get_clipboard() + clipboard
+
+		sublime.set_clipboard(clipboard)
