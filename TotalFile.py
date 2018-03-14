e74b6b476690b2fafa80f227aaf0a0d981be0eb1
Richard Hyde <github@richardhyde.net>
2018-03-14 10:14:36 +0000

    Handle $ and € as prefixs to the amounts, or no currency prefix at all

--- TotalFile.py
+++ TotalFile.py
@@ -3,43 +3,54 @@ import re
 import sublime, sublime_plugin
 
 class TotalFileCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		cleaned = []
-		numbers = []
+  def run(self, edit):
+    cleaned = []
+    numbers = []
+    totalCurrency = ""
 
-		region = sublime.Region(0, self.view.size());
+    region = sublime.Region(0, self.view.size());
 
-		for lineRegion in self.view.lines(region):
-			line = self.view.substr(lineRegion)
+    for lineRegion in self.view.lines(region):
+      line = self.view.substr(lineRegion)
 
-			if (line == ""):
-				break
+      if (line == ""):
+        break
 
-			try:
-				m = re.match(u"£\s*([0-9\.,]{1,9})\s*(.*)", line, re.U)
+      try:
+        m = re.match(u"([£$€])\s*([0-9\.,]{1,9})\s*(.*)", line, re.U)
 
-				if (m):
-					cost = float(m.group(1).strip(' '))
-					numbers.append(cost)
-					desc = m.group(2)
+        if (not m):
+          m = re.match(u"\s*([0-9\.,]{1,9})\s*(.*)", line, re.U)
 
-					cleaned.append(u"£{0:>9.2f} {1}".format(cost, desc))
-				else:
-					cleaned.append(line)
-			except ValueError:
-				cleaned.append(line)
+        if (m):
+          currency = ""
+          gl = len(m.groups())
+          if gl >= 3:
+            currency = m.group(1).strip(' ')
+            if len(totalCurrency) == 0:
+              totalCurrency = currency
 
-		total = sum(numbers)
-		if (len(cleaned) > 0):
-			while cleaned[-1].strip() == '':
-				del cleaned[-1]
+          cost = float(m.group(gl-1).strip(' '))
+          numbers.append(cost)
+          desc = m.group(gl)
 
-		cleaned.append("")
-		cleaned.append(u"£{0:>9.2f} Total".format(total))
-		cleaned = '\n'.join(cleaned)
+          cleaned.append(u"{0}{1:>9.2f} {2}".format(currency, cost, desc))
+        else:
+          cleaned.append(line)
+      except ValueError:
+        cleaned.append(line)
 
-		#edit = self.view.begin_edit("")
-		self.view.erase(edit, region)
-		self.view.insert(edit, 0, cleaned)
-		#self.view.end_edit(edit)
+    total = sum(numbers)
+    if (len(cleaned) > 0):
+      while cleaned[-1].strip() == '':
+        del cleaned[-1]
+
+    cleaned.append("")
+    cleaned.append(u"{0}{1:>9.2f} Total".format(totalCurrency, total))
+    cleaned = '\n'.join(cleaned)
+
+    #edit = self.view.begin_edit("")
+    self.view.erase(edit, region)
+    self.view.insert(edit, 0, cleaned)
+    #self.view.end_edit(edit)
 
