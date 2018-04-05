1ec8e9c75b09e8b0719275ff95950404183bf36c
Richard Hyde <email@richardhyde.net>
2018-04-05 11:54:25 +0100

    Fixed for Sublime Text 3

--- LoadURL.py
+++ LoadURL.py
@@ -1,18 +1,29 @@
+import urllib
 import sublime, sublime_plugin
-import urllib2
 
 class LoadUrlCommand(sublime_plugin.TextCommand):
 	def loadURL(self, url):
 		sublime.status_message("Loading " + url)
 
-		f = urllib2.urlopen(url)
-		data = f.read()
+		f = urllib.request.urlopen(url)
 
-		if (data != ""):
-			outputView = sublime.active_window().new_file()
-			newEdit = outputView.begin_edit()
-			outputView.insert(newEdit, 0, data)
-			outputView.end_edit(newEdit)
+		if (f.status == 200):
+			data = f.read()
+
+			if (data != ""):
+				outputView = sublime.active_window().new_file()
+				outputView.set_scratch(True)
+				outputView.run_command("append", {"characters": data.decode('UTF-8').replace("\r\n", "\n"), "force": True, "scroll_to_end": False})
+				#outputView.set_syntax_file(self.view.settings().get('syntax'))
+
+				sublime.status_message(str(len(data)))
+			else:
+				sublime.status_message("No Data Returned")	
+		else:
+			sublime.status_message("Returned status code " + str(f.reason))
 
 	def run(self, edit):
 		self.view.window().show_input_panel("Enter a URL to load:", "", self.loadURL, None, None)
+
+
+## http://feeds.macrumors.com/MacRumors-All
